b7ab919b68f761ebfa6fc6a54e6ddc7f827a2a4f
richfjiq <rfjiq1986@gmail.com>
2022-06-21 08:52:56 -0500

    chore: add size selector

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -4,8 +4,10 @@ import {
   CardActionArea,
   CardMedia,
   Grid,
+  Link,
   Typography,
 } from '@mui/material';
+import NextLink from 'next/link';
 import React, { FC, useMemo, useState } from 'react';
 import { IProduct } from '../../interfaces';
 
@@ -31,15 +33,19 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseLeave={() => setIsHovered(false)}
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            className="fadeIn"
-            image={productImage}
-            alt={product.title}
-            onLoad={() => console.log('cargo')}
-          />
-        </CardActionArea>
+        <NextLink href="/product/slug" passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className="fadeIn"
+                image={productImage}
+                alt={product.title}
+                onLoad={() => console.log('cargo')}
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
       <Box sx={{ mt: 1 }} className="fadeIn">
