4f4d3a144e25c1c7923487091294b9c8c787048d
richfjiq <rfjiq1986@gmail.com>
2022-06-28 04:33:06 -0500

    fin seccion 16

---
+++ context/auth/AuthProvider.tsx
@@ -0,0 +1,100 @@
+import { FC, ReactNode, useEffect, useReducer } from 'react';
+import Cookies from 'js-cookie';
+import axios, { AxiosError } from 'axios';
+
+import { tesloApi } from '../../api';
+import { IUser } from '../../interfaces';
+import { AuthContext, authReducer } from './';
+
+interface Props {
+  children: ReactNode;
+}
+
+export interface AuthState {
+  isLoggedIn: boolean;
+  user?: IUser;
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+  isLoggedIn: false,
+  user: undefined,
+};
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const checkToken = async () => {
+    try {
+      const { data } = await tesloApi.get('/user/validate-token');
+      const { token, user } = data;
+      Cookies.set('token', token);
+      dispatch({ type: '[Auth] - Login', payload: user });
+    } catch (error) {
+      Cookies.remove('token');
+    }
+  };
+
+  const loginUser = async (
+    email: string,
+    password: string
+  ): Promise<boolean> => {
+    try {
+      const { data } = await tesloApi.post('/user/login', { email, password });
+      const { token, user } = data;
+      Cookies.set('token', token);
+      dispatch({ type: '[Auth] - Login', payload: user });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  const registerUser = async (
+    name: string,
+    email: string,
+    password: string
+  ): Promise<{ hasError: boolean; message?: string }> => {
+    try {
+      const { data } = await tesloApi.post('/user/register', {
+        name,
+        email,
+        password,
+      });
+      const { token, user } = data;
+      Cookies.set('token', token);
+      dispatch({ type: '[Auth] - Login', payload: user });
+      return {
+        hasError: false,
+      };
+      // TODO: return
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        return {
+          hasError: true,
+          message: error.message,
+        };
+      }
+
+      return {
+        hasError: true,
+        message: 'No se pudo crear el usuario - intente de nuevo',
+      };
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        loginUser,
+        registerUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,63 +1,135 @@
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import NextLink from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/router';
+import {
+  Box,
+  Button,
+  Chip,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { ErrorOutline } from '@mui/icons-material';
+
 import { AuthLayout } from '../../components/layouts';
+import { tesloApi } from '../../api';
+import { validations } from '../../utils';
+import { AuthContext } from '../../context';
+
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const RegisterPage = () => {
+  const router = useRouter();
+  const { registerUser } = useContext(AuthContext);
+  const [showError, setShowError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onRegisterForm = async ({ name, email, password }: FormData) => {
+    setShowError(false);
+    const { hasError, message } = await registerUser(name, email, password);
+
+    if (hasError) {
+      setShowError(true);
+      setErrorMessage(message!);
+      setTimeout(() => setShowError(false), 3000);
+      return;
+    }
+
+    router.replace('/');
+  };
+
   return (
     <AuthLayout title="Ingresar">
-      <Box sx={{ width: 350, padding: '10px 20px' }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Typography variant="h1" component="h1">
-              Crear cuenta
-            </Typography>
-          </Grid>
+      <form onSubmit={handleSubmit(onRegisterForm)} noValidate>
+        <Box sx={{ width: 350, padding: '10px 20px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography variant="h1" component="h1">
+                Crear cuenta
+              </Typography>
+              <Chip
+                label="No se puede registrar, el correo ya existe."
+                color="error"
+                icon={<ErrorOutline />}
+                className="fadeIn"
+                sx={{ display: showError ? 'flex' : 'none' }}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Nombre Completo" variant="filled" fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Nombre Completo"
+                variant="filled"
+                fullWidth
+                {...register('name', {
+                  required: 'Este campo es requerido.',
+                })}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField label="Correo" variant="filled" fullWidth />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                type="email"
+                label="Correo"
+                variant="filled"
+                fullWidth
+                {...register('email', {
+                  required: 'Este campo es requerido.',
+                  validate: validations.isEmail,
+                })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
+            <Grid item xs={12}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                variant="filled"
+                fullWidth
+                {...register('password', {
+                  required: 'Este campo es requerido',
+                  minLength: { value: 6, message: 'Minimo 6 caracteres' },
+                })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Grid>
 
-          <Grid item xs={12}>
-            <TextField
-              label="Confirmar Contraseña"
-              type="password"
-              variant="filled"
-              fullWidth
-            />
-          </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                color="secondary"
+                className="circular-btn"
+                size="large"
+                fullWidth
+              >
+                Registrar
+              </Button>
+            </Grid>
 
-          <Grid item xs={12}>
-            <Button
-              color="secondary"
-              className="circular-btn"
-              size="large"
-              fullWidth
-            >
-              Registrar
-            </Button>
+            <Grid item xs={12} display="flex" justifyContent="end">
+              <NextLink href="/auth/login">
+                <Link underline="always">¿Ya tienes cuenta?</Link>
+              </NextLink>
+            </Grid>
           </Grid>
-
-          <Grid item xs={12} display="flex" justifyContent="end">
-            <NextLink href="/auth/login">
-              <Link underline="always">¿Ya tienes cuenta?</Link>
-            </NextLink>
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>{' '}
+      </form>
     </AuthLayout>
   );
 };
